ccea83e1cd96d2ecfc384445ebd383a1a7268997
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-04-27 22:27:39 +0300

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/PhilipHassialis/learning-go-hello-world-web/pkg/config"
+	"github.com/PhilipHassialis/learning-go-hello-world-web/pkg/handlers"
+	"github.com/PhilipHassialis/learning-go-hello-world-web/pkg/render"
+	"github.com/alexedwards/scs/v2"
+)
+
+const PORT_NUMBER = ":8080"
+
+var app config.AppConfig // global visibility in all of main package
+var session *scs.SessionManager
+
+// main is the entry point for the program
+func main() {
+
+	app.InProduction = false // set it to true for production
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
+	// http.HandleFunc("/divide", handlers.Repo.Divide)
+	fmt.Println("Server is listening on port", PORT_NUMBER)
+	// http.ListenAndServe(PORT_NUMBER, nil)
+
+	srv := &http.Server{
+		Addr:    PORT_NUMBER,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,72 @@
+package handlers
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/PhilipHassialis/learning-go-hello-world-web/pkg/config"
+	"github.com/PhilipHassialis/learning-go-hello-world-web/pkg/models"
+	"github.com/PhilipHassialis/learning-go-hello-world-web/pkg/render"
+)
+
+// Repo is the Repository for the handlers
+var Repo *Repository
+
+// Repository is the repository for the handlers
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new Repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{App: a}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "This is the home page!")
+
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remoteIP", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remoteIP")
+	stringMap["remoteIP"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
+func (m *Repository) Divide(w http.ResponseWriter, r *http.Request) {
+	var x float32 = 100
+	var y float32 = 10
+	f, err := divideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "cannot divide by zero")
+	} else {
+		fmt.Fprintf(w, "%.2f divided by %.2f is %.2f", x, y, f)
+	}
+
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return x / y, nil
+}
